3ae2661699e3088af6313a600bb9ba7dec4d54e6
Olivier_MBP <olivier.catanese@gmail.com>
2022-11-25 16:27:34 +0100

    Création page projet avec barre de progression_ choix du projet et affichage des infos en ajax
    Changement css et js barre de progression pour conflit bootstrap

--- assets/app.js
+++ assets/app.js
@@ -20,26 +20,26 @@ let i = 1;
 
 function changerEtape(){
     if(i<=4){
-        document.querySelector('.circle:nth-of-type(' + i + ')').classList.add("pending");
+        document.querySelector('.circle1:nth-of-type(' + i + ')').classList.add("pending");
     }
     if(i>=2 && i<=5){
-        document.querySelector('.circle:nth-of-type(' + (i-1) + ')').classList.remove("pending");
-        document.querySelector('.circle:nth-of-type(' + (i-1) + ')').classList.add("done");
+        document.querySelector('.circle1:nth-of-type(' + (i-1) + ')').classList.remove("pending");
+        document.querySelector('.circle1:nth-of-type(' + (i-1) + ')').classList.add("done");
         if(i<5){
-            document.querySelector('.bar:nth-of-type(' + (i-1) + ')').classList.add("pending");
+            document.querySelector('.bar1:nth-of-type(' + (i-1) + ')').classList.add("pending");
         }
     }
     if(i>=3 && i<=5){
-        document.querySelector('.bar:nth-of-type(' + (i-2) + ')').classList.remove("pending");
-        document.querySelector('.bar:nth-of-type(' + (i-2) + ')').classList.add("done");
+        document.querySelector('.bar1:nth-of-type(' + (i-2) + ')').classList.remove("pending");
+        document.querySelector('.bar1:nth-of-type(' + (i-2) + ')').classList.add("done");
     }
     // Effacement de la barre progression
     if(i == 6){
         i = 0
         for(let index=1; index<=4; index++){
-            document.querySelector('.circle:nth-of-type(' + (index) + ')').classList.remove("done");
+            document.querySelector('.circle1:nth-of-type(' + (index) + ')').classList.remove("done");
             if(index<4){
-                document.querySelector('.bar:nth-of-type(' + (index) + ')').classList.remove("done");
+                document.querySelector('.bar1:nth-of-type(' + (index) + ')').classList.remove("done");
             }
         }
     }
